0c9699db3598ff2987d2c5411edb84acfa7f226e
Zoom <xabou2010@gmail.com>
2026-08-08 09:19:28 -0600

    refactor: remove useless code

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,20 +1,3 @@
-/*
- * Guestbook Service
- * Copyright (C) 2026 Zoom
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-
 import { app } from "./app";
 
 app.listen(process.env.PORT || 3000);
